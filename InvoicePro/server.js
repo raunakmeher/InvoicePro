66aab7998344ca8a116f85ef3024373338d304d7
raunakmeher <161629991+raunakmeher@users.noreply.github.com>
2025-07-30 21:07:15 +0530

    Update server.js

--- InvoicePro/server.js
+++ InvoicePro/server.js
@@ -18,7 +18,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173', // Development
     'http://localhost:3000', // Alternative dev port
-    'https://invoice-pro-wine.vercel.app', 
+    invoice-pro-git-main-raunaks-projects-2ec5b3d0.vercel.app
     'https://*.vercel.app', // All Vercel subdomains
   ],
   credentials: true,
