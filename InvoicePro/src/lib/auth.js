2838b29f505e17295b6bb6cb934ae0c2df75b046
raunakmeher <161629991+raunakmeher@users.noreply.github.com>
2025-08-02 00:39:45 +0530

    Update auth.js

--- InvoicePro/src/lib/auth.js
+++ InvoicePro/src/lib/auth.js
@@ -10,10 +10,16 @@ export async function register({ email, password }) {
 }
 
 export async function login({ email, password }) {
+   console.log('Registering with URL:', `${API_URL}/register`);
+  console.log('Registering with data:', { email, password });
   const res = await fetch(`${API_URL}/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password }),
   });
-  return res.json();
+    console.log('Register response status:', res.status);
+  const data = await res.json();
+  console.log('Register response data:', data);
+  
+  return data;
 } 
